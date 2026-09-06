3dc8bf82e9a4dc0401947c4947a43555308dc474
Deeya <deeya3280@gmail.com>
2026-09-06 12:28:08 +0530

    update structures.h

--- include/structures.h
+++ include/structures.h
@@ -11,16 +11,36 @@ typedef struct {
     int busId;
     char busNumber[20];
     int capacity;
+    int availableSeats;
 } Bus;
 
 typedef struct {
     int routeId;
     char routeName[50];
+    char stops[200];
 } Route;
 
 typedef struct {
     int driverId;
     char name[50];
+    char phone[15];
 } Driver;
+
+typedef struct {
+    int seatNumber;
+    int busId;
+    int studentId;
+    char status[15];       // Available / Booked
+    char preference[15];   // Window / Front / Any
+} Seat;
+
+typedef struct {
+    int scheduleId;
+    int busId;
+    int routeId;
+    char startTime[10];
+    char endTime[10];
+    char dayType[15];      // Full Day / Half Day
+} Schedule;
 
 #endif
