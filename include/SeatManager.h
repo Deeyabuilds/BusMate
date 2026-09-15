7322e73b5b8dfe8c7b99a292f7d7fe539e6fad4c
gunjan bisht <gunjanbisht2008@gmail.com>
2026-09-15 20:48:12 +0530

    refactor: align SeatManager with central structures.h

--- include/SeatManager.h
+++ include/SeatManager.h
@@ -3,19 +3,20 @@
 
 #include "structures.h"
 #include "file_handler.h"
+#include <stdbool.h>
 
-// Prototypes from original binary implementation
-void init_seats_file();
-int book_seat_binary(int seat_id, const char *passenger_name);
-int is_seat_booked(int seat_id);
-
-// Preference Allocation Prototypes
 #define TOTAL_ROWS 10
 #define SEATS_PER_ROW 4
 #define TOTAL_SEATS (TOTAL_ROWS * SEATS_PER_ROW)
 
+// Binary File Functions
+void init_seats_file(void);
+int book_seat_binary(int seat_id, int bus_id, int student_id, const char *preference);
+int is_seat_booked(int seat_id, int bus_id);
+
+// System Allocation Functions
 void init_seat_system(void);
-int allocate_seat(int student_id, const char *name, SeatPreference pref);
+int allocate_seat(int student_id, const char *name, const char *pref);
 bool cancel_seat(int student_id);
 void display_bus_occupancy(void);
 
